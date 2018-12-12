91a29c8efc218df2debd0d14ef9ee686aff7ce53
Adam Jakubowski <adamjski@gmail.com>
2018-12-12 16:56:14 +0100

    zadanie1

---
+++ src/zadaniaDomowe/Main.java
@@ -0,0 +1,13 @@
+package zadaniaDomowe;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ZadaniaTablice zadania = new ZadaniaTablice();
+
+        ZadaniaTablice.zadanie1(new int[]{2,4,5,7});
+
+    }
+
+
+}
---
+++ src/zadaniaDomowe/ZadaniaTablice.java
@@ -0,0 +1,12 @@
+package zadaniaDomowe;
+
+public class ZadaniaTablice {
+
+    public static void zadanie1(int[]tablica){
+        for (int i = 0; i <tablica.length ; i++) {
+            System.out.println(tablica[i]);
+
+        }
+    }
+
+}
